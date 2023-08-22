cb607e095c69826461ed58a64a84d3374eb3d3d4
Quan Tong <61928209+nhatquantong@users.noreply.github.com>
2023-08-22 21:20:54 +0700

    Initial Commit

---
+++ trivia-app/trivia_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  trivia_appApp.swift
+//  trivia-app
+//
+//  Created by Nhật Quân on 22/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct trivia_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
